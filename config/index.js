aa91a5be543b11ce545757f71e779c0557594d75
Allen Garvey <allen.garvey@gmail.com>
2018-05-19 13:06:53 -0400

    Remove more unneede dependencies and build scripts

--- config/index.js
+++ config/index.js
@@ -9,17 +9,5 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
     productionSourceMap: true
-  },
-  dev: {
-    env: require('./dev.env'),
-    port: 3004,
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '',
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    cssSourceMap: false
   }
 }
