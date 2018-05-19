70b5fe02843cb0fb5874fe85b41a6a49d2c07dcf
Allen Garvey <allen.garvey@gmail.com>
2018-05-19 12:55:18 -0400

    Remove all color picker components except for Photoshop

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import Compact from './components/Compact.vue'
-import Grayscale from './components/Grayscale.vue'
-import Material from './components/Material.vue'
-import Slider from './components/Slider.vue'
-import Swatches from './components/Swatches.vue'
 import Photoshop from './components/Photoshop.vue'
-import Sketch from './components/Sketch.vue'
-import Chrome from './components/Chrome.vue'
 import Alpha from './components/common/Alpha.vue'
 import Checkboard from './components/common/Checkboard.vue'
 import EditableInput from './components/common/EditableInput.vue'
@@ -15,14 +8,7 @@ import ColorMixin from './mixin/color.js'
 
 const VueColor = {
   version: '2.4.6',
-  Compact,
-  Grayscale,
-  Material,
-  Slider,
-  Swatches,
   Photoshop,
-  Sketch,
-  Chrome,
   Alpha,
   Checkboard,
   EditableInput,
