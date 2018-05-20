6d2ca3fccca3480d51dec0aca8bcdf6fa4b729b8
Allen Garvey <allen.garvey@gmail.com>
2018-05-20 00:31:51 -0400

    Remove unused exports from index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import Photoshop from './components/Photoshop.vue'
-import EditableInput from './components/common/EditableInput.vue'
-import Hue from './components/common/Hue.vue'
-import Saturation from './components/common/Saturation.vue'
-import ColorMixin from './mixin/color.js'
 
 const VueColor = {
   version: '2.4.6',
-  Photoshop,
-  EditableInput,
-  Hue,
-  Saturation,
-  ColorMixin
+  Photoshop
 }
 
 module.exports = VueColor
